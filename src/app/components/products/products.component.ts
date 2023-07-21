9e14ea0d126e2d97863636c7e7e0928191fcf1bc
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-20 22:18:53 -0500

    services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 
+import { StoreService } from '../../services/store.service';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -48,8 +50,12 @@ export class ProductsComponent {
     },
   ];
 
+  constructor(private storeService: StoreService) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
   onAddToShoppingCart(product: Product) {
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 }
